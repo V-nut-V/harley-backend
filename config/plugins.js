ed74406a933e9f40e925ceebee3696493c82380b
Harley Lin <harley9387@gmail.com>
2023-01-02 22:07:46 +1300

    disable graphql playground in production

--- config/plugins.js
+++ config/plugins.js
@@ -12,17 +12,5 @@ module.exports = ({ env }) => ({
       },
     },
   },
-  graphql: {
-    config: {
-      endpoint: '/graphql',
-      shadowCRUD: true,
-      playgroundAlways: true,
-      depthLimit: 7,
-      amountLimit: 100,
-      apolloServer: {
-        tracing: false,
-      },
-    },
-  },
   // ...
 });
